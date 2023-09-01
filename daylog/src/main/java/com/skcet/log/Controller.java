cce65cf776fda02482d2dcc57430ba3d02691619
Ragul4 <108716512+Ragul4@users.noreply.github.com>
2023-09-01 10:08:33 +0530

    Add files via upload

---
+++ daylog/src/main/java/com/skcet/log/Controller.java
@@ -0,0 +1,24 @@
+package com.skcet.log;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class Controller {
+	Logger logger=LoggerFactory.getLogger(Controller.class);
+	
+	@GetMapping("/test")
+	public String hi() {
+		logger.trace("A TRACE Message");
+		logger.debug("A DEBUG Message");
+		logger.info("An INFO Message");
+		logger.warn("A WARN Message");
+		logger.error("'An ERROR Message");
+		return "Hello Scaler!";
+	}
+}
---
+++ daylog/src/test/java/com/skcet/log/DaylogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skcet.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaylogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
